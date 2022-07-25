1a53ba183259a814076657fe7d54f0e433e30195
Guilherme Silva <gfsilva@hotmail.com>
2022-07-25 15:33:52 -0300

    rev

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
 
   def academy
   end
+
+  def modal
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'academy', to: "pages#academy"
+  get 'modal', to: "pages#modal"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
